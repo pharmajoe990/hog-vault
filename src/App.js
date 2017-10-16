c8ea8120e91fda2e3e317eff4d861bc7e8718256
Tim Roper <tim.roper@myob.com>
2017-10-16 15:01:39 +1100

    Create new vault button added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
 import './App.css';
 import VaultLink from './VaultLink';
-import { Grid, PageHeader, Row, Col } from 'react-bootstrap';
+import { Grid, PageHeader, Row, Col, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Grid>
-          <PageHeader>Welcome to Hog Vault
-            <br />
-            <small>Please select from the vaults below to contribute</small>
-          </PageHeader>
+          <Row>
+            <PageHeader>Welcome to Hog Vault
+              <br />
+              <small>Please select from the vaults below to contribute</small>
+            </PageHeader>
+          </Row>
+          <Row>
           <Grid>
             <Row>
               <Col xs={12} lg={4}><VaultLink description={"Titan team breakfast"} amount={34.50} /></Col>
@@ -23,6 +26,10 @@ class App extends Component {
               <Col xs={12} lg={4}><VaultLink description={"Slush Fund"} amount={340.22} /></Col>
             </Row>
           </Grid>
+          </Row>
+          <Row>
+            <Button bsStyle="default">Create new vault</Button>
+          </Row>
         </Grid>
       </div>
     );
